7c7876116b12099a2a7b5644b59dd962883c639f
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-09-12 14:18:12 +0200

    Create class of club data

---
+++ src/main/java/pl/csanecki/webflux/club/Club.java
@@ -0,0 +1,24 @@
+package pl.csanecki.webflux.club;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
+public class Club {
+
+    @MongoId
+    private String id;
+    private String name;
+
+    public Club(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
