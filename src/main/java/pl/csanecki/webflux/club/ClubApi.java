c2c313cbcf573bab60650ed061e96b55d2a4d92b
Sanecki Cezary <Cezary.Sanecki@snt-is.com>
2020-09-12 16:00:21 +0200

    Add to annotation produces stream of JSONs

--- src/main/java/pl/csanecki/webflux/club/ClubApi.java
+++ src/main/java/pl/csanecki/webflux/club/ClubApi.java
@@ -1,6 +1,7 @@
 package pl.csanecki.webflux.club;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,7 +16,7 @@ public class ClubApi {
         this.clubRepo = clubRepo;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Club> getAllClubs() {
         return clubRepo.findAll();
     }
